e0434e4873661ca2104f376a5320107881fb7ce2
HeyMorraz <heylingmorrraz123@gmail.com>
2023-07-27 14:39:06 -0600

    Avance final

--- 3-column-preview-card-component-main/jsmain.js
+++ 3-column-preview-card-component-main/jsmain.js
@@ -1,7 +1,24 @@
 //guardar el estado (columna activa)
 
+
 let columnaActiva = 1
 
 //selecccionar las columnas
 
-const columnas = document.querySelectorAll("Card__articles")
+const columnas = document.querySelectorAll('.Card__articles')
+
+//escuchar los clicks en cada una de ellas
+columnas .forEach((Card__articles, indice) => {
+    Card__articles.addEventListener("click", function(){
+       cambiarColumna(indice)
+    })
+   
+})
+
+//cambiar el estado de las columnas
+function cambiarColumna(indice){
+    columnas[columnaActiva].classList.remove("activa")
+    columnas[indice].classList.add("activa")
+    columnaActiva = indice
+}
+
